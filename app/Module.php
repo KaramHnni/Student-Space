fadf2c1f7424595dd87f88715d3f882a74486f74
Henni Karam <karam.tenes@gmail.com>
2018-09-01 12:27:58 +0100

    add speciality foreign key and relationship method in module table

--- app/Module.php
+++ app/Module.php
@@ -9,6 +9,9 @@ class Module extends Model
     public function absences(){
         return $this->hasMany('App\Absence','module_id','id');
     }
+    public function speciality(){
+        return $this->hasOne('\App\Speciality','id','speciality_id');
+    }
 
     public function getFullNameAttribute(){
         return "$this->name$this->semestre" ;
--- app/Speciality.php
+++ app/Speciality.php
@@ -9,4 +9,8 @@ class Speciality extends Model
     public function departement(){
         return $this->hasOne('\App\Departement','id','departement_id');
     }
+
+    public function modules(){
+        return $this->hasMany('App\Departement','speciality_id','id');
+    }
 }
